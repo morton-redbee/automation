db4be6a9060d0487dcc6da92c4974c33fd28b8bd
Morton <santiago.figueroa@redb.ee>
2017-08-14 18:49:39 -0300

    fix seleccion

--- start_test.js
+++ start_test.js
@@ -7,39 +7,37 @@ test_type = command_line_parameters[2];
 environment_option = command_line_parameters[3];
 
 if (test_type) {
+	console.log('se seleccionó el test ' + test_type);
 	collection_file = '/' + test_type + '.postman_collection.json';
-	if (!fs.existsSync(collection_file)) {
-		console.log ('el test indicado no existe.');
-		process.exit();
-	}
+	console.log(collection_file);
 } else {
-	console.log ('no se definió el test a ejecutar');
+	console.log ('no se definió el test a ejecutar. Test abortado.');
 	process.exit();
 }
 
+collection_path = require('path').dirname(require.main.filename) + collection_file;
 
+if (!fs.existsSync(collection_path)) {
+	console.log ('el test indicado no existe. Test abortado.');
+	process.exit();
+}
 
 if (environment_option) {
 	environment_file = '/' + environment_option + '.postman_environment.json';
-	if (!fs.existsSync(environment_file)) {
-		console.log ('la configuracion del entorno indicado no existe.');
-		process.exit();
-	}
+	console.log(environment_file);
 } else {
-	console.log ('no se definió el entorno a utilizar');
+	console.log ('no se definió el entorno a utilizar. Test abortado.');
+	process.exit();
+}
+
+environment_path = require('path').dirname(require.main.filename) + environment_file;
+
+if (!fs.existsSync(environment_path)) {
+	console.log ('la configuracion del entorno indicado no existe. Test abortado.');
 	process.exit();
 }
 
 parameters = require('./parameters.json');
-
-console.log(test_type);
-
-
-collection_path = require('path').dirname(require.main.filename) + collection_file;
-
-String.prototype.escapeSpecialChars = function() {
-    return this.replace(/,/g, ",\n").replace(/{\"/g, "{\n\"").replace('[', '[\n').replace('},\n', '\n},\n').replace('}]', '}\n]\n');
-};
 
 function setParameters() {		
 	if (parameters.site) {
@@ -51,6 +49,12 @@ function setParameters() {
 }
 
 function editDtxTest(fileName) {
+
+	String.prototype.escapeSpecialChars = function() {
+    	return this.replace(/,/g, ",\n").replace(/{\"/g, "{\n\"").replace('[', '[\n').replace('},\n', '\n},\n').replace('}]', '}\n]\n');
+	};
+
+
 	var file = require(fileName);
 	
 	file.item[1].request.url =  '{{base_url}}:{{port_replication}}/replication/site/' + site;
@@ -94,6 +98,31 @@ function editDtxTest(fileName) {
 	console.log('Iteration Data file was edited successfully.');
 }
 
+function editStxTest(fileName) {
+	
+	String.prototype.escapeSpecialChars = function() {
+    	return this.replace(/,/g, ",\n").replace(/{\"/g, "{\n\"").replace('[', '[\n');
+	};
+
+	var file = require(fileName);
+
+	var raw_payment_json = JSON.parse(file.item[1].request.body.raw);
+
+	string_payment_raw = JSON.stringify(raw_payment_json);
+
+	escaped_string_payment_raw = string_payment_raw.escapeSpecialChars();
+
+	file.item[1].request.body.raw = escaped_string_payment_raw;
+
+	fs.writeFileSync(fileName, JSON.stringify(file, null, 2), function (err) {
+	if (err) return console.log(err);
+		console.log(JSON.stringify(file));
+		console.log('writing to ' + fileName);
+	});
+
+	console.log('Iteration Data file was edited successfully.');
+}
+
 // Test que prueba realizar un pago a través de la API
 function runTest() {	
 	newman.run({
@@ -112,7 +141,7 @@ setParameters();
 if (test_type.indexOf('dtx') > -1) {
 	editDtxTest(collection_path);
 } else if (test_type.indexOf('stx') > -1) {
-	console.log('aca iria la edicion de stx');
+	editStxTest(collection_path);
 }
 runTest();
 
