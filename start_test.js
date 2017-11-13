9a23317ee77fc2e540217add420673692de6a580
morton-redbee <santiago.figueroa@redb.ee>
2017-11-13 16:52:45 -0300

    fix

--- start_test.js
+++ start_test.js
@@ -168,8 +168,8 @@ function editStxTest(fileName) {
 function runTest() {	
 	newman.run({
 	    	collection: require('.' + collection_file),
-		environment: require('.' + environment_file), 
-		delayRequest: 3000,
+		environment: require('.' + environment_file), 	
+		delayRequest: 2000,
 		reporters: ['junit', 'cli'],
 		reporter : { junit : { export : '/build/newman-test-result/newman-test-results_' + program.results + '.xml' } } 
 		}, function (err) {
