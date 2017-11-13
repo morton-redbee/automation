edca95d1f7950fe0699320cb8850f2eb10502dbf
morton-redbee <santiago.figueroa@redb.ee>
2017-11-13 13:51:11 -0300

    fix

--- start_test.js
+++ start_test.js
@@ -169,7 +169,7 @@ function runTest() {
 	newman.run({
 	    	collection: require('.' + collection_file),
 		environment: require('.' + environment_file),
-		delayRequest: 2000,   				
+		delayRequest: 1000,   				
 		reporters: ['junit', 'cli'],
 		reporter : { junit : { export : '/build/newman-test-result/newman-test-results_' + program.results + '.xml' } } 
 		}, function (err) {
