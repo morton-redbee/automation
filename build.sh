f44ab52ea53d28592e531ca4a830214f7aee8575
Juan Ignacio Rego <JuanIgnacio.Rego@gmail.com>
2018-12-10 12:55:49 -0300

    Agregadas pruebas de PMC

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMG="lapp-dvde004:5000/automation:$(git describe --abbrev=0 --tags 2>/dev/null || echo 'latest')"
+IMG="registry.dev.redbee.io/automation:$(git describe --abbrev=0 --tags 2>/dev/null || echo 'latest')"
 
 echo "Building $IMG"
 docker build -t "$IMG" . && \
